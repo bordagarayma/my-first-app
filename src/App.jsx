5d8f58f0f92bebe32f86d96e73684e8e6f05f0f1
Maria Bordagaray <bordagaray.maria@gmail.com>
2026-03-09 12:08:07 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import * as d3 from "d3";
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>How are youuuu?</p>
+    </>
+  )
+}
+
+export default App
